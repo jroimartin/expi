35f33aa4c7189fc44ff9024b82a3a78ab1b91d28
Roi Martin <jroi.martin@gmail.com>
2022-12-13 08:57:54 +0100

    examples/led_no_macros: add example without expi_macros

---
+++ examples/led_no_macros/src/main.rs
@@ -0,0 +1,70 @@
+//! Blinking LED.
+
+#![feature(naked_functions, panic_info_message)]
+#![no_std]
+#![no_main]
+
+use core::arch::asm;
+use core::panic::PanicInfo;
+
+use expi::cpu::time;
+use expi::gpio;
+use expi::uart;
+use expi::{print, println};
+
+/// The LED is connected to GPIO26.
+const GPIO_LED: u32 = 26;
+
+/// Kernel main function.
+#[no_mangle]
+extern "C" fn kernel_main() {
+    // Initialize the UART.
+    if uart::init().is_err() {
+        return;
+    }
+
+    println!("expi");
+
+    gpio::set_function(GPIO_LED, gpio::Function::Output).unwrap();
+    loop {
+        gpio::set(&[GPIO_LED]).unwrap();
+        time::delay(1_000_000);
+        gpio::clear(&[GPIO_LED]).unwrap();
+        time::delay(1_000_000);
+    }
+}
+
+/// Kernel entrypoint.
+#[link_section = ".entry"]
+#[no_mangle]
+#[naked]
+unsafe extern "C" fn _start() -> ! {
+    asm!(
+        r#"
+                ldr x5, =0x80000
+                mov sp, x5
+                bl kernel_main
+            1:
+                b 1b
+        "#,
+        options(noreturn),
+    )
+}
+
+/// Panic handler.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    print!("\n\n!!! PANIC !!!\n\n");
+
+    if let Some(location) = info.location() {
+        print!("{}:{}", location.file(), location.line());
+    }
+
+    if let Some(message) = info.message() {
+        println!(": {}", message);
+    } else {
+        println!();
+    }
+
+    loop {}
+}
