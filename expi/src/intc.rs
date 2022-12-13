7817886cdcbb935546d45e7857d3973f3dac04a4
Roi Martin <jroi.martin@gmail.com>
2022-12-13 19:41:38 +0100

    expi/intc: add fiq control and pending register access

--- expi/src/intc.rs
+++ expi/src/intc.rs
@@ -21,22 +21,62 @@ use crate::mmio;
 /// ```
 ///
 /// [/arch/arm/boot/dts/bcm2837.dtsi]: https://github.com/raspberrypi/linux/blob/770d94882ac145c81af72e9a37180806c3f70bbd/arch/arm/boot/dts/bcm2837.dtsi
-const IRQ_BASE: usize = 0xb200;
+const INTC_BASE: usize = 0xb200;
 
-/// Base address of the IRQ enable registers.
-const IRQEN_BASE: usize = IRQ_BASE + 0x10;
+/// Base address of the interrupt pending registers.
+const INTPEND_BASE: usize = INTC_BASE;
 
-/// Base address of the IRQ disable registers.
-const IRQDIS_BASE: usize = IRQ_BASE + 0x1c;
+/// Address of the FIQ control register.
+const FIQCTL: usize = INTC_BASE + 0xc;
 
-/// Provides the required information to configure a specific peripheral.
-struct RegBit {
-    /// Register index.
-    idx: usize,
+/// Base address of the interrupt enable registers.
+const INTEN_BASE: usize = INTC_BASE + 0x10;
 
-    /// Bit position.
-    bit: u32,
+/// Base address of the interrupt disable registers.
+const INTDIS_BASE: usize = INTC_BASE + 0x1c;
+
+/// Interrupt register.
+pub enum Reg {
+    /// Basic interrupt register.
+    Basic,
+
+    /// GPU interrupt register 1.
+    Gpu1,
+
+    /// GPU interrupt register 2.
+    Gpu2,
 }
+
+/// Index of an interrupt control register (i.e. interrupt enable/disable
+/// register).
+struct RegCtlIdx(usize);
+
+impl From<Reg> for RegCtlIdx {
+    fn from(reg: Reg) -> RegCtlIdx {
+        match reg {
+            Reg::Gpu1 => RegCtlIdx(0),
+            Reg::Gpu2 => RegCtlIdx(1),
+            Reg::Basic => RegCtlIdx(2),
+        }
+    }
+}
+
+/// Index of a pending register.
+struct RegPendingIdx(usize);
+
+impl From<Reg> for RegPendingIdx {
+    fn from(reg: Reg) -> RegPendingIdx {
+        match reg {
+            Reg::Basic => RegPendingIdx(0),
+            Reg::Gpu1 => RegPendingIdx(1),
+            Reg::Gpu2 => RegPendingIdx(2),
+        }
+    }
+}
+
+/// Provides the register and bit position required to configure a given
+/// peripheral.
+struct RegBit(Reg, u32);
 
 /// BCM2837 peripherals.
 pub enum Peripheral {
@@ -47,21 +87,56 @@ pub enum Peripheral {
 impl From<Peripheral> for RegBit {
     fn from(peripheral: Peripheral) -> RegBit {
         match peripheral {
-            Peripheral::GPIO => RegBit { idx: 1, bit: 20 },
+            Peripheral::GPIO => RegBit(Reg::Gpu2, 20),
         }
     }
 }
 
-/// Enables the interrupts of the provided peripheral.
-pub fn enable(peripheral: Peripheral) {
-    let reg_bit: RegBit = peripheral.into();
-    let addr = IRQEN_BASE + reg_bit.idx * 4;
-    unsafe { mmio::write(addr, 1 << reg_bit.bit) };
+/// Represents an FIQ source.
+struct FIQSource(u32);
+
+impl From<Peripheral> for FIQSource {
+    fn from(peripheral: Peripheral) -> FIQSource {
+        match peripheral {
+            Peripheral::GPIO => FIQSource(52),
+        }
+    }
 }
 
-/// Disables the interrupts of the provided peripheral.
+/// Enables interrupts for the provided peripheral.
+pub fn enable(peripheral: Peripheral) {
+    let reg_bit = RegBit::from(peripheral);
+    let reg_idx = RegCtlIdx::from(reg_bit.0);
+    let addr = INTEN_BASE + reg_idx.0 * 4;
+    unsafe { mmio::write(addr, 1 << reg_bit.1) };
+}
+
+/// Disables interrupts for the provided peripheral.
 pub fn disable(peripheral: Peripheral) {
-    let reg_bit: RegBit = peripheral.into();
-    let addr = IRQDIS_BASE + reg_bit.idx * 4;
-    unsafe { mmio::write(addr, 1 << reg_bit.bit) };
+    let reg_bit = RegBit::from(peripheral);
+    let reg_idx = RegCtlIdx::from(reg_bit.0);
+    let addr = INTDIS_BASE + reg_idx.0 * 4;
+    unsafe { mmio::write(addr, 1 << reg_bit.1) };
+}
+
+/// Returns if a given peripheral has a pending interrupt.
+pub fn pending(peripheral: Peripheral) -> bool {
+    let reg_bit = RegBit::from(peripheral);
+    let reg_idx = RegPendingIdx::from(reg_bit.0);
+    let addr = INTPEND_BASE + reg_idx.0 * 4;
+    let reg = unsafe { mmio::read(addr) };
+    reg & (1 << reg_bit.1) != 0
+}
+
+/// Select which interrupt source can generate a FIQ to the ARM. Only a single
+/// interrupt can be selected.
+pub fn enable_fiq(peripheral: Peripheral) {
+    let source = FIQSource::from(peripheral);
+    let reg = (1 << 7) | (source.0 & 0b11_1111);
+    unsafe { mmio::write(FIQCTL, reg) };
+}
+
+/// Disable FIQs.
+pub fn disable_fiq() {
+    unsafe { mmio::write(FIQCTL, 0) };
 }
