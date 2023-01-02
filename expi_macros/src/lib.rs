09987017a617d1915b45f35427dca5d3bb3d8e02
Roi Martin <jroi.martin@gmail.com>
2023-01-02 21:08:07 +0100

    expi_macros: use lowercase assembly

--- expi_macros/src/lib.rs
+++ expi_macros/src/lib.rs
@@ -108,27 +108,27 @@ pub fn exception_handler(_attr: TokenStream, item: TokenStream) -> TokenStream {
 
     let handler_code = format!(
         r#"
-            stp x0, x1, [SP, #-16]!
-            stp x2, x3, [SP, #-16]!
-            stp x4, x5, [SP, #-16]!
-            stp x6, x7, [SP, #-16]!
-            stp x8, x9, [SP, #-16]!
-            stp x10, x11, [SP, #-16]!
-            stp x12, x13, [SP, #-16]!
-            stp x14, x15, [SP, #-16]!
-            stp lr, xzr, [SP, #-16]!
+            stp x0, x1, [sp, #-16]!
+            stp x2, x3, [sp, #-16]!
+            stp x4, x5, [sp, #-16]!
+            stp x6, x7, [sp, #-16]!
+            stp x8, x9, [sp, #-16]!
+            stp x10, x11, [sp, #-16]!
+            stp x12, x13, [sp, #-16]!
+            stp x14, x15, [sp, #-16]!
+            stp lr, xzr, [sp, #-16]!
 
             bl {fname_c}
 
-            ldp lr, xzr, [SP], #16
-            ldp x14, x15, [SP], #16
-            ldp x12, x13, [SP], #16
-            ldp x10, x11, [SP], #16
-            ldp x8, x9, [SP], #16
-            ldp x6, x7, [SP], #16
-            ldp x4, x5, [SP], #16
-            ldp x2, x3, [SP], #16
-            ldp x0, x1, [SP], #16
+            ldp lr, xzr, [sp], #16
+            ldp x14, x15, [sp], #16
+            ldp x12, x13, [sp], #16
+            ldp x10, x11, [sp], #16
+            ldp x8, x9, [sp], #16
+            ldp x6, x7, [sp], #16
+            ldp x4, x5, [sp], #16
+            ldp x2, x3, [sp], #16
+            ldp x0, x1, [sp], #16
 
             eret
         "#
