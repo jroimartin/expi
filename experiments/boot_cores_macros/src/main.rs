b48fd9a57c33cb0fbfa0f1700be079aa41a3cd8d
Roi Martin <jroi.martin@gmail.com>
2023-01-03 01:27:58 +0100

    experiments/boot_cores_macros: add experiment

---
+++ experiments/boot_cores_macros/src/main.rs
@@ -0,0 +1,16 @@
+//! Boot all cores.
+
+#![feature(naked_functions, panic_info_message)]
+#![no_std]
+#![no_main]
+
+use expi::cpu::mp;
+use expi::print;
+
+use expi_macros::entrypoint_mp;
+
+/// Kernel main function.
+#[entrypoint_mp]
+fn kernel_main(_dtb_ptr32: u32) {
+    print!("{}", mp::core_id());
+}
