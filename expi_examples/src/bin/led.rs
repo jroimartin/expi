70aa87cee4594b1c6e55b232d223ae60974c79a6
Roi Martin <jroi.martin@gmail.com>
2023-01-10 00:44:21 +0100

    expi_examples/led: increase delay between set and clear

--- expi_examples/src/bin/led.rs
+++ expi_examples/src/bin/led.rs
@@ -21,8 +21,8 @@ fn kernel_main(_dtb_ptr32: u32) {
     pin_led.set_function(Function::Output);
     loop {
         pin_led.set();
-        time::delay(1_000_000);
+        time::delay(100_000_000);
         pin_led.clear();
-        time::delay(1_000_000);
+        time::delay(100_000_000);
     }
 }
